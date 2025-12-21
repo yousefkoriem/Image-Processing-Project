607c157b2e68d7489b3756af4d04d759b38fc35e
Yousef Koriem <koriemyousef@gmail.com>
2025-12-21 02:18:51 +0200

    edits & fixes

---
+++ filters/noise/salt_and_pepper.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+def Salt_and_Pepper(image, salt_prob=0.01, pepper_prob=0.01):
+    noisy_image = image.copy()
+    salt_mask = np.random.random(image.shape) < salt_prob
+    noisy_image[salt_mask] = 255
+    pepper_mask = np.random.random(image.shape) < pepper_prob
+    noisy_image[pepper_mask] = 0
+    return noisy_image
